d09f21dac9c1a057e9a4581850b8bc332a93c0ce
hongjie <hongjie.lim@beyondsoft.com>
2023-06-06 17:46:15 +0800

    add tracing span stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration, startup::run, telemetry::get_subcriber,
+    telemetry::init_subscriber,
+};
 
+/// Compose multiple layers into a `tracing`'s subscriber.
+///
+/// # Implementation Notes
+///
+/// We are using `impl Subscriber` as return type to avoid having to
+/// spell out the actual type of the returned subscriber, which is
+/// indeed quite complex.
+/// We need to explicitly call out that the returned subscriber is
+/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
+/// later on.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // logging subscriber setup
+    let subscriber = get_subcriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
+    // configuration setup
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address).expect("Failed to bind to port.");
+
+    // db setup
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
+
+    // startup
     run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
 use tracing;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -15,16 +16,15 @@ pub async fn subscribe(
     db_pool: web::Data<PgPool>,
 ) -> impl Responder {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name,
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in database",
-        request_id
-    );
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -38,6 +38,7 @@ pub async fn subscribe(
     // We use `get_ref` to get an immutable reference to the `PgConnection`
     // wrapped by `web::Data`.
     .execute(db_pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
