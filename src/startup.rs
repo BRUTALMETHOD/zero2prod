507f870fa0b651772d319d7cd1376581edc1e462
hj <hongjie@hongjie.dev>
2023-06-08 11:47:26 +0800

    finish telemetry

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,8 @@
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer, Result};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, hello, subscribe};
 
@@ -10,7 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .service(hello)
             .route("/subscriptions", web::post().to(subscribe))
             .route("/health_check", web::get().to(health_check))
