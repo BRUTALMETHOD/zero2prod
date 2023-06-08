85a63006c34fe37eac04ed3a4cd1ab639877a237
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2023-06-08 22:32:32 +0800

    change configurations in build

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use zero2prod::{
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // logging subscriber setup
-    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    let subscriber = get_subscriber("zero2prod".into(), "debug".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     // configuration setup
