9d0da7ec60a53394571dc78aa583347614ccfcb6
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2023-11-24 22:25:03 +0800

    10.2 fin

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -98,20 +98,30 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, &pool)
-        .await
-        .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$aron2id$v=19$m=15000,t=2,p=1$\
+    gZiV/MigPc22E1AH/Jh1Hw$\
+    CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, &pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
 
-    let current_span = tracing::Span::current();
     spawn_blocking_with_tracing(move || {
-        current_span.in_scope(|| verify_password_hash(expected_password_hash, credentials.password))
+        verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking tasks..")
     .map_err(PublishError::UnexpectedError)??;
 
-    Ok(user_id)
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
 }
 
 #[tracing::instrument(
