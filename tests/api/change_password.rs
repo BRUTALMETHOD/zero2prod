f86747511d3740996c960c32e20ba0d2e1e18246
BRUTALMETHOD <brutalmethod@users.noreply.github.com>
2023-12-07 00:15:58 +0800

    current password must be valid

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -45,3 +45,23 @@ async fn new_password_fields_must_match() {
         html_page.contains("You entered two different new passwords - the field values must match")
     );
 }
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    //Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+    // Act 1 - Login
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    }))
+    .await;
+    // Act 2 - Change Password
+    let response = app.post_change_password(&serde_json::json!({"current_password": &wrong_password,"new_password": &new_password, "new_password_check": &new_password})).await;
+    assert_is_redirect_to(&response, "/admin/password");
+    // Assert
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains("The current password is incorrect."));
+}
